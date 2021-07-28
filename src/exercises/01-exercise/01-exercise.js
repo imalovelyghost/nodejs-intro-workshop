d6ec692f55037243b3cff7583ccc4883bf4214a6
imalovelyghost <imalovelyghost13@gmail.com>
2021-07-28 11:04:04 +0200

    first exercise done

--- src/exercises/01-exercise/01-exercise.js
+++ src/exercises/01-exercise/01-exercise.js
@@ -3,6 +3,11 @@
  *    and use it in the `add()` function to add it to the 2 parameters
  */
 
+// import BASE_NUM from "./constants";
+ const BASE_NUM =require("./constants");
+//  let BASE_NUM=require("./constants");BASE_NUM - bring only the constant
+
+
 /**
  * 2. Use module.exports = { ... } to export 2 functions:
  *
@@ -10,6 +15,18 @@
  *    Takes 2 parameters `a` and `b` and returns their sum
  *    add to the `BASE_NUM` constant variable imported
  *
+ *
  * 2.2. subtract(a, b)
  *    Takes 2 parameters `a` and `b` and returns `b` - `a`
  */
+
+module.exports= {
+    add: function (a,b) {
+      
+      return a+b+BASE_NUM;
+    },
+    subtract: function (a,b) {
+return b-a;
+
+    },
+};
